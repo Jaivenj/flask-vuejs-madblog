835674ee716ae3f791fb35c7ee2cb23f6eeb874c
Javen <646398022@qq.com>
2020-05-13 00:43:47 +0800

    5. 个人主页与用户头像

--- front-end/src/components/Profile.vue
+++ front-end/src/components/Profile.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="container">
     <el-container>
-      <el-header>个人信息</el-header>
+      <el-header class="border ">
+        <H1>个人信息</H1>
+      </el-header>
       <el-container class="border">
         <el-aside width="250px">
-          
           <div class="block">
             <el-avatar shape="circle" :size="200" :fit="fit"
               src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"></el-avatar>
           </div>
-          <H1 >
-            <font size="6">{{user.username}}</font>
-          </H1>
-          <el-button icon="el-icon-edit" type="primary" round >Edit Profile</el-button>
+          <router-link v-if="$route.params.id == sharedState.user_id" v-bind:to="{ name: 'EditProfile' }">
+            <el-button icon="el-icon-edit" type="primary" round>Edit Profile</el-button>
+          </router-link>
 
         </el-aside>
         <el-main>
-          Main
-            <el-divider><i class="el-icon-postcard"></i></el-divider>
-          </el-main>
+          <H1>
+            <font size="5">{{user.username}}</font>
+          </H1>
+          <el-row :gutter="20">
+            <el-col :span="3.5">
+              <div class="el-icon-news"> Member since :</div>
+            </el-col>
+            <el-col :span="6">
+              <div> {{ $moment(user.member_since).format('LLL') }}</div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="3.5">
+              <div class="el-icon-view"> Last seen :</div>
+            </el-col>
+            <el-col :span="6">
+              <div>{{ $moment(user.last_seen).fromNow() }}</div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="3.5">
+              <div class="el-icon-message"> Verified User</div>
+            </el-col>
+            <el-col :span="3">
+              <a v-bind:href="'mailto:' + user.email">{{ user.email }}</a>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="3.5">
+              <div class="el-icon-location-outline" v-if="user.location">Location:</div>
+            </el-col>
+            <el-col :span="6">
+              <div>{{user.location}}</div>
+            </el-col>
+          </el-row>
+          <el-divider><i class="el-icon-postcard"></i></el-divider>
+          <div class="text item">
+            <p>生命犹如一片绿叶，随着时间的流逝，慢慢变的枯黄，但他的叶脉还是那么清晰可见。
+            </p>
+          </div>
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -78,30 +116,57 @@
 
     color: #333;
     text-align: center;
-    line-height: 60px;
+    line-height: 0px;
   }
 
   .el-aside {
 
     color: #333;
     text-align: center;
-    line-height: 10px;
-
+    line-height: 50px;
   }
 
   .el-main {
 
     color: #333;
-    text-align: center;
-    line-height: 100px;
+    text-align: left;
+    line-height: 15px;
+
   }
 
   .container {
-    padding: 100px;
+    padding: 50px 10% 200px 10%;
   }
-  .border{
-      border-radius: 4px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .20);
-      padding: 40px
+
+  .border {
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .20);
+    padding: 10px
   }
+
+  .el-row {
+    margin-bottom: 20px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  .text {
+    font-size: 16px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
 </style>
