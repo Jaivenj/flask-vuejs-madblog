44de7ae49d9f630cae3348d085df81e8db3911dd
jaivenj <646398022@qq.com>
2020-05-22 17:41:56 +0800

    7.粉丝与点击关注，User页面做完，还差Post列表

---
+++ front-end/src/components/pagination.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:"pagination.vue",
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
